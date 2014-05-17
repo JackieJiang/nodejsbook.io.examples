fb2f367ccd4a26954d15524e30e6a2b9b5f66a31
Jackie.Jiang <jianga@nbsunsoft.com>
2014-05-17 10:24:50 +0800

    http.get({host:url}, function(){})中的host:是什么意思？

--- hour03/example01/app.js
+++ hour03/example01/app.js
@@ -4,6 +4,7 @@ var http = require('http'),
 function fetchPage(url) {
   var start = new Date();
   http.get({ host: url }, function(res) {
+  	// 这个host:是什么意思？
     console.log("Got response from: " + url);
     console.log('Request took:', new Date() - start, 'ms');
   });
